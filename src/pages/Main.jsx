3863e379041aa40f24811e02a86abe6be6f44b34
suga-ming <kby9901@naver.com>
2022-08-26 17:57:20 +0900

    html&css

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import "../pages/Main.css";
-import List from "../components/List";
-import Input from "../components/Input";
+// import "../pages/Main.css";
+import "../pages/Main.scss";
 
 const Main = () => {
   return (
     <div className="container">
       <div className="content">
         <div className="title">To Do List</div>
+        <div className="list-area">
+          <input
+            type="text"
+            className="list"
+            placeholder="할 일을 입력해주세요."
+            autoComplete="off"
+          ></input>
+          <button className="button">+</button>
+        </div>
+        <div className="list-item-area">
+          <div className="list-item"></div>
+        </div>
       </div>
-      {/* <List /> */}
-      {/* <Input /> */}
     </div>
   );
 };
